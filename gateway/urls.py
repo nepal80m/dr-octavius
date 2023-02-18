ff3d47fc9f77a84f8f3bafb20de416bb4df9f904
Asim Nepal <gnepal80m@gmail.com>
2023-02-18 20:22:36 +0545

    Changed check nid url

--- gateway/urls.py
+++ gateway/urls.py
@@ -4,5 +4,5 @@ from gateway.views import GetDocumentsView, CheckNINView
 app_name = "gateway"
 urlpatterns = [
     path("documents/", GetDocumentsView.as_view(), name="documents"),
-    path("check/<uuid:NIN>/", CheckNINView.as_view(), name="documents"),
+    path("check/<str:NIN>/", CheckNINView.as_view(), name="check_nin"),
 ]
