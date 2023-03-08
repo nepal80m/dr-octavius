54530c9e6925e14e9f8e23a29cf3e2efd31edb95
Asim Nepal <gnepal80m@gmail.com>
2023-03-08 23:19:19 +0545

    modified to return the list of history in descending order

--- history/views.py
+++ history/views.py
@@ -10,7 +10,9 @@ class ActivityHistoryListCreateView(ListCreateAPIView):
     serializer_class = ActivityHistorySerializer
 
     def get_queryset(self):
-        return ActivityHistory.objects.filter(user=self.request.user)
+        return ActivityHistory.objects.filter(user=self.request.user).order_by(
+            "-created_at"
+        )
 
     def perform_create(self, serializer):
         activity_history = serializer.save(user=self.request.user)
